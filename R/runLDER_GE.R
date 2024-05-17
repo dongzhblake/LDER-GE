a6378c616e760ccf4b2505cd769cf0e31046b6ed
dongzhblake <70220157+dongzhblake@users.noreply.github.com>
2024-05-17 14:21:04 -0400

    Update runLDER_GE.R

--- R/runLDER_GE.R
+++ R/runLDER_GE.R
@@ -14,22 +14,19 @@
 #'
 #'
 runLDER_GE <- function(assoc, n.gwas, path, LD.insample=T,  n.ld,method='lder', cores=10, a=NULL, type='jack',size_num=200){
-  rough <- !LD.insample
-  twostage = rough
   A=unlist(strsplit(list.files(path,pattern="INFO"),".txt"));B=unlist(strsplit(A,"SNPINFO"));C=max(as.numeric(B),na.rm=T)
-  #stats <- extract_stats(C,assoc=assoc,assoc2=NULL,ldpath=ldpath,ldpath.shrink=ldpath.shrink,n.ld=n.ld,impute_missing=T)
   library(parallel)
   ldpath <- ldpath.shrink <- path
   print("matching summary statistics with LD panel")
   stats <- mclapply(0:C,get.stats,assoc=assoc,ldpath=ldpath,ldpath.shrink=ldpath.shrink,mc.cores=cores,n.ld=n.ld)
   if(method=='lder'){
-    res <- lder(stats=stats,n.gwas=n.gwas,a=NULL,rough=rough,cores=cores,twostage=twostage,type=type)
+    res <- lder(stats=stats,n.gwas=n.gwas,a=NULL,rough=rough,cores=cores,twostage=!LD.insample,type=type)
     return(res)
   }else if(method=='ldsc'){
     res <- ldsc(stats=stats,n.gwas=n.gwas,a=NULL,cores=cores,twostage=F,type=type)
     return(res)
   }else if(method=='both'){
-    res1 <- lder(stats=stats,n.gwas=n.gwas,a=NULL,rough=rough,cores=cores,twostage=twostage,type=type)
+    res1 <- lder(stats=stats,n.gwas=n.gwas,a=NULL,rough=rough,cores=cores,twostage=!LD.insample,type=type)
     res2 <- ldsc(stats=stats,n.gwas=n.gwas,a=NULL,cores=cores,twostage=F,type=type)
     return(list(lder=res1,ldsc=res2))
   }
